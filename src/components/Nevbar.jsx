b14f54d498b7c13cae2e1c02db92303d742d0e81
Anas saifi <saifianas771@gmail.com>
2025-04-03 18:40:23 +0530

    make navbar responsive andd the preloading page

---
+++ src/components/Nevbar.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+
+function Nevbar({ manuOpen, setmanuOpen }) {
+    useEffect(() => {
+        document.body.style.overflowY = manuOpen ? "hidden" : "";
+    }, [manuOpen])
+    return (
+        <div className='fixed top-0 w-full z-40  bg-[rgb(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg' >
+            <div className='max-w-5xl mx-auto px-4'>
+
+                <div className='flex justify-between items-center h-16'>
+
+                    <a href="#home" className='font-mono font-bold text-white hover:scale-105 transition-all duration-500 ease-in-out' >
+                        anas.<span className='text-blue-500'>saifi</span>
+                    </a>
+
+                    <div className='w-7 h-5 relative cursor-pointer z-40 md:hidden'
+                        onClick={() => setmanuOpen(!manuOpen)}>
+                        &#9776;
+
+                    </div>
+
+                    <div className='hidden md:flex items-center space-x-5 font-mono' >
+                        <a href="#Home" className='hover:text-gray-200 hover:scale-105 transition-all duration-500 ease-in-out' >Home</a>
+                        <a href="#About" className='hover:text-gray-200 hover:scale-105 transition-all duration-500 ease-in-out'>About</a>
+                        <a href="#Contact" className='hover:text-gray-200 hover:scale-105 transition-all duration-500 ease-in-out'>Contect</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Nevbar
---
+++ src/components/loadingScrean.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+
+function LoadingScrean({ toglevisibleLoading }) {
+    const [text, setText] = useState("");
+    const fulltext = "<Hello World! />"
+
+    useEffect(() => {
+        let index = 0;
+        const interval = setInterval(() => {
+            setText(fulltext.substring(0, index))
+            // console.log(index)
+            index++;
+            if (index > fulltext.length) {
+                clearInterval(interval);
+                setTimeout(() => {
+
+                    toglevisibleLoading();
+                }, 500);
+            }
+        }, 100);
+        return () => clearInterval(interval);
+    }, [toglevisibleLoading])
+
+    return (
+        <div className='flex w-[100vw] h-[100vh] items-center justify-center'>
+            <div className="flex flex-col items-center w-[65%] lg:w-[20%] gap-y-5">
+
+                <div className="mt-4 text-2xl font-mono bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+                    {text}
+                    <span className='ml-2 text-white animate-blink'>|</span>
+                </div>
+                <div className="w-full bg-gray-200 rounded-full h-0.5">
+                    <div
+                        className="bg-gradient-to-r from-blue-500 to-indigo-600 h-[100%] rounded-full transition-all duration-400 shadow-2xl"
+                        style={{
+                            width: `${(text.length / fulltext.length) * 100}%`,
+                        }}
+                    ></div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default LoadingScrean
