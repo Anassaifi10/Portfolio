f2ef2e610e5b7be01e8b36b098126287e494c216
Anas saifi <saifianas771@gmail.com>
2025-04-07 11:19:17 +0530

    setup for tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Include .jsx and .tsx for React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
